9e79f1679cf16b46c10de37667f431ddb1341e04
Nihad Kalathingal <nihadkalathingal@gmail.com>
2020-02-18 23:36:05 -0500

    add notebook and format

---
+++ Prog_Assi_01.py
@@ -0,0 +1,36 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Bank_Predictions.csv')
+
+# ------ Part-1: Data preprocessing ----------
+
+# Encoding categorical data
+
+# Splitting the dataset into the Training and Test sets
+
+# Feature Scaling
+
+# ------- Part-2: Build the ANN --------
+
+# import keras library and packages
+
+# Initializing the ANN
+
+# Adding the input layer and the first hidden layer
+
+# Adding second hidden layer
+
+# Adding output layer
+
+# Compiling the ANN
+
+# Fitting the ANN to the training set
+
+# Predicting the Test set results
+
+# Making the confusion Matrix
+
